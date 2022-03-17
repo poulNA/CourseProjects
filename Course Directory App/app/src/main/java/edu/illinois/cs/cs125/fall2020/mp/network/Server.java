80831cadb59a2b1a97da4669b401785f4ac3b416
Paul Jeong <pauljeong25@gmail.com>
2022-03-17 12:51:26 -0400

    Course Directory App
    Created search bar and rating features using Java and Android Studio.

---
+++ Course Directory App/app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/network/Server.java
@@ -0,0 +1,246 @@
+package edu.illinois.cs.cs125.fall2020.mp.network;
+
+import androidx.annotation.NonNull;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.illinois.cs.cs125.fall2020.mp.application.CourseableApplication;
+import edu.illinois.cs.cs125.fall2020.mp.models.Rating;
+import edu.illinois.cs.cs125.fall2020.mp.models.Summary;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+/**
+ * Development course API server.
+ *
+ * <p>Normally you would run this server on another machine, which the client would connect to over
+ * the internet. For the sake of development, we're running the server right alongside the app on
+ * the same device. However, all communication between the course API client and course API server
+ * is still done using the HTTP protocol. Meaning that eventually it would be straightforward to
+ * move this server to another machine where it could provide data for all course API clients.
+ *
+ * <p>You will need to add functionality to the server for MP1 and MP2.
+ */
+public final class Server extends Dispatcher {
+  @SuppressWarnings({"unused", "RedundantSuppression"})
+  private static final String TAG = Server.class.getSimpleName();
+
+  private final Map<String, String> summaries = new HashMap<>();
+
+  private final Map<Summary, Map<String, Rating>> ratings = new HashMap<>();
+
+  private MockResponse getSummary(@NonNull final String path) {
+    String[] parts = path.split("/");
+    if (parts.length != 2) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+    }
+
+    String summary = summaries.get(parts[0] + "_" + parts[1]);
+    if (summary == null) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+    }
+    return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(summary);
+  }
+
+  private MockResponse getCourse(@NonNull final String path) {
+    final int x = 4;
+    String[] parts = path.split("/");
+    if (parts.length != x) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+    }
+    Summary summary = new Summary(parts[0], parts[1], parts[2], parts[3], "");
+    String description = courses.get(summary);
+    if (description == null) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+    }
+    return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(description);
+  }
+
+  private MockResponse getRating(@NonNull final String path) {
+    final int x = 5;
+    String[] parts = path.split("/|\\?client=");
+    if (parts.length != x) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+    }
+    Summary summary = new Summary(parts[0], parts[1], parts[2], parts[3], "");
+    if (courses.get(summary) == null) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+    }
+    /*if (parts[x - 1] != null) {
+      Rating notRated = new Rating((parts[x - 1]), Rating.NOT_RATED);
+      String rating2 = "";
+      try {
+        rating2 = mapper.writeValueAsString(notRated);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(rating2);
+    }*/
+    if (ratings.get(summary) == null) {
+      Rating notRated = new Rating((parts[x - 1]), Rating.NOT_RATED);
+      String rating2 = "";
+      try {
+        rating2 = mapper.writeValueAsString(notRated);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(rating2);
+    } else if (ratings.get(summary).get(parts[x - 1]) == null) {
+      Rating notRated = new Rating((parts[x - 1]), Rating.NOT_RATED);
+      String rating2 = "";
+      try {
+        rating2 = mapper.writeValueAsString(notRated);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(rating2);
+    } else {
+      Rating rating = ratings.get(summary).get(parts[x - 1]);
+      String rating1 = "";
+      try {
+        rating1 = mapper.writeValueAsString(rating);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(rating1);
+    }
+  }
+
+  private String theString = "";
+
+  private MockResponse postRating(
+      @NonNull final RecordedRequest request, @NonNull final String path) {
+    final int x = 4;
+    theString = request.getBody().readUtf8();
+    String[] parts = path.split("/");
+    if (parts.length != x) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+    }
+    String[] chunk = parts[3].split("\\?client=");
+    if (chunk.length != 2) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+    }
+    String num = chunk[0];
+    String id = chunk[1];
+    Summary summary = new Summary(parts[0], parts[1], parts[2], num, "");
+    if (courses.get(summary) == null) {
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+    }
+    Rating rating1 = new Rating();
+    try {
+      rating1 = mapper.readValue(theString, Rating.class);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+    }
+
+    Map<String, Rating> insideMap = ratings.getOrDefault(summary, new HashMap<>());
+    insideMap.put(id, rating1);
+    ratings.put(summary, insideMap);
+
+    return new MockResponse()
+        .setResponseCode(HttpURLConnection.HTTP_MOVED_TEMP)
+        .setHeader("Location", "/rating/" + path);
+  }
+
+  @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+  private final Map<Summary, String> courses = new HashMap<>();
+
+  @NonNull
+  @Override
+  public MockResponse dispatch(@NonNull final RecordedRequest request) {
+    try {
+      String path = request.getPath();
+      if (path == null || request.getMethod() == null) {
+        return new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+      } else if (path.equals("/") && request.getMethod().equalsIgnoreCase("HEAD")) {
+        return new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK);
+      } else if (path.startsWith("/summary/")) {
+        return getSummary(path.replaceFirst("/summary/", ""));
+      } else if (path.startsWith("/course/")) {
+        return getCourse(path.replaceFirst("/course/", ""));
+      } else if (path.startsWith("/rating/")) {
+        if (request.getMethod().equals("POST")) {
+          return postRating(request, path.replaceFirst("/rating/", ""));
+        } else if (request.getMethod().equals("GET")) {
+          return getRating(path.replaceFirst("/rating/", ""));
+        }
+      }
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new MockResponse().setResponseCode(HttpURLConnection.HTTP_INTERNAL_ERROR);
+    }
+  }
+
+  private static boolean started = false;
+
+  /**
+   * Start the server if has not already been started.
+   *
+   * <p>We start the server in a new thread so that it operates separately from and does not
+   * interfere with the rest of the app.
+   */
+  public static void start() {
+    if (!started) {
+      new Thread(Server::new).start();
+      started = true;
+    }
+  }
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  private Server() {
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    loadSummary("2020", "fall");
+    loadCourses("2020", "fall");
+
+    try {
+      MockWebServer server = new MockWebServer();
+      server.setDispatcher(this);
+      server.start(CourseableApplication.SERVER_PORT);
+
+      String baseUrl = server.url("").toString();
+      if (!CourseableApplication.SERVER_URL.equals(baseUrl)) {
+        throw new IllegalStateException("Bad server URL: " + baseUrl);
+      }
+    } catch (IOException e) {
+      throw new IllegalStateException(e.getMessage());
+    }
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  private void loadSummary(@NonNull final String year, @NonNull final String semester) {
+    String filename = "/" + year + "_" + semester + "_summary.json";
+    String json =
+        new Scanner(Server.class.getResourceAsStream(filename), "UTF-8").useDelimiter("\\A").next();
+    summaries.put(year + "_" + semester, json);
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  private void loadCourses(@NonNull final String year, @NonNull final String semester) {
+    String filename = "/" + year + "_" + semester + ".json";
+    String json =
+        new Scanner(Server.class.getResourceAsStream(filename), "UTF-8").useDelimiter("\\A").next();
+    try {
+      JsonNode nodes = mapper.readTree(json);
+      for (Iterator<JsonNode> it = nodes.elements(); it.hasNext(); ) {
+        JsonNode node = it.next();
+        Summary course = mapper.readValue(node.toString(), Summary.class);
+        courses.put(course, node.toPrettyString());
+      }
+    } catch (JsonProcessingException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+}
